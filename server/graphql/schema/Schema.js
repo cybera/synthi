42c47896050bf2a406f5e087491b4fb3679c79fd
David Ackerman <david.s.ackerman@gmail.com>
2018-05-08 11:39:59 -0600

    convert to preferred schema def format of file upload

--- server/graphql/schema/Schema.js
+++ server/graphql/schema/Schema.js
@@ -1,110 +1,92 @@
+const { makeExecutableSchema } = require('graphql-tools')
+
 const graphql = require('graphql')
 const neo4j = require('../../neo4j/connection')
 
-// define the Dataset type for graphql
-const DatasetType = new graphql.GraphQLObjectType({
-  name: 'dataset',
-  description: 'a dataset item',
-  fields: {
-    id: {type: graphql.GraphQLInt},
-    name: {type: graphql.GraphQLString}
-  }
-})
+const typeDefs = `
+type Dataset {
+  id: Int!
+  name: String!
+}
 
-// define the queries of the graphql Schema
-const query = new graphql.GraphQLObjectType({
-  name: 'DatasetQuery',
-  fields: {
-    dataset: {
-      type: new graphql.GraphQLList(DatasetType),
-      args: {
-        id: {
-          type: graphql.GraphQLInt
-        }
-      },
-      resolve: (_, {id}) => {
-        const session = neo4j.session()
+type Query {
+  dataset(id: Int): [Dataset]
+}
 
-        var query = [`MATCH (n:Dataset) RETURN n.name AS name, ID(n) AS id`]
-        if (id) {
-          query = [`MATCH (n:Dataset) 
-                    WHERE ID(n) = $id 
-                    RETURN 
-                      n.name AS name, 
-                      ID(n) AS id`, { id: id }]
-        }
-        
-        return session.run(...query).then(result => {
-          const datasets = result.records.map(record => record.toObject())
-          return datasets
-        }).catch(e => {
-          return []
-        }).then(result => {
-          session.close()
-          return result
-        })
+type Mutation {
+  createDataset(name: String!): Dataset
+  deleteDataset(id: Int!): Dataset
+}
+
+schema {
+  query: Query
+  mutation: Mutation
+}
+`
+
+const resolvers = {
+  Query: {
+    dataset(_, { id }) {
+      const session = neo4j.session()
+
+      var query = [`MATCH (n:Dataset) RETURN n.name AS name, ID(n) AS id`]
+      if (id) {
+        query = [`MATCH (n:Dataset) 
+                  WHERE ID(n) = $id 
+                  RETURN 
+                    n.name AS name, 
+                    ID(n) AS id`, { id: id }]
       }
+      
+      return session.run(...query).then(result => {
+        const datasets = result.records.map(record => record.toObject())
+        return datasets
+      }).catch(e => {
+        return []
+      }).then(result => {
+        session.close()
+        return result
+      }) 
     }
-  }
-})
+  },
+  Mutation: {
+    createDataset(_, {name}) {
+      const session = neo4j.session()
 
-// define the mutations of the graphql Schema
-const mutation = new graphql.GraphQLObjectType({
-  name: 'DatasetMutation',
-  fields: {
-    createDataset: {
-      type: new graphql.GraphQLList(DatasetType),
-      args: {
-        name: {
-          type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-        }
-      },
-      resolve: (_, {name}) => {
-        const session = neo4j.session()
-
-        return session.run(`CREATE (d:Dataset { name: $name }) 
-                            RETURN ID(d) AS id, d.name AS name`, 
-                           { name: name }).then(result => {
-          return result.records.map(record => record.toObject())
-        }).catch(e => {
-          return []
-        }).then(result => {
-          session.close()
-          return result
-        })
-      }
+      return session.run(`CREATE (d:Dataset { name: $name }) 
+                          RETURN ID(d) AS id, d.name AS name`, 
+                         { name: name }).then(result => {
+        return result.records.map(record => record.toObject())[0]
+      }).catch(e => {
+        return []
+      }).then(result => {
+        session.close()
+        return result
+      })
     },
-    deleteDataset: {
-      type: new graphql.GraphQLList(DatasetType),
-      args: {
-        id: {
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        }
-      },
-      resolve: (_, {id}) => {
-        const session = neo4j.session()
+    deleteDataset(_, {id}) {
+      const session = neo4j.session()
 
-        return session.run(`MATCH (d:Dataset)
-                            WHERE ID(d) = $id
-                            WITH d, ID(d) AS id, d.name AS name
-                            DETACH DELETE d
-                            RETURN id, name`, 
-                           { id: id }).then(result => {
-          return result.records.map(record => record.toObject())
-        }).catch(e => {
-          return []
-        }).then(result => {
-          session.close()
-          return result
-        })
-      }
+      return session.run(`MATCH (d:Dataset)
+                          WHERE ID(d) = $id
+                          WITH d, ID(d) AS id, d.name AS name
+                          DETACH DELETE d
+                          RETURN id, name`, 
+                        { id: id }).then(result => {
+        return result.records.map(record => record.toObject())[0]
+      }).catch(e => {
+        return []
+      }).then(result => {
+        session.close()
+        return result
+      })
     }
   }
+}
+
+const executableSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
 })
 
-// creates and exports the GraphQL Schema
-module.exports = new graphql.GraphQLSchema({
-  query,
-  mutation
-})
-
+module.exports = executableSchema
