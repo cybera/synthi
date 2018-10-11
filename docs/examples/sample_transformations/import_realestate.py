4b0b630bb976eca2d2bad5dded5a289276e90f3d
David Ackerman <david.s.ackerman@gmail.com>
2018-10-11 15:22:35 -0600

    commenting on examples

--- docs/examples/sample_transformations/import_realestate.py
+++ docs/examples/sample_transformations/import_realestate.py
@@ -1,5 +1,13 @@
 # Credit: https://stackoverflow.com/questions/35371043/use-python-requests-to-download-csv
 
+# Example:
+#
+# Notice that there are no "dataset_input()" calls in this one. That's because it
+# creates its dataset completely from a file streamed from AWS. This is a really
+# simple example that shows how you could enter a "scraped" dataset into ADI. In
+# this case, we're able to access the .csv format directly, so we don't really have
+# to do any scraping.
+
 import csv
 import requests
 import pandas as pd
