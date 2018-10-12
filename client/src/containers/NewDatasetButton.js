4ae24dc0982cc6fe128e766bec0b59db078456ad
David Ackerman <david.s.ackerman@gmail.com>
2018-10-11 21:23:41 -0600

    select new dataset when first created

--- client/src/containers/NewDatasetButton.js
+++ client/src/containers/NewDatasetButton.js
@@ -27,23 +27,43 @@ const styles = theme => ({
     marginLeft: 10
   }
 })
+ 
+class NewDatasetButton extends React.Component 
+{
+  constructor(props) {
+    super()
+    this.handleClick.bind(this)
+  }
 
-const NewDatasetButton = (props) => {
-  const { classes } = props
+  handleClick = (mutation) => {
+    const { navigation } = this.props
+    mutation().then(results => {
+      const { createDataset } = results.data
+      navigation.selectDataset(createDataset.id)
+    })
+  }
+  
+  render() {
+    const { classes } = this.props
 
-  return (
-    <Mutation 
-      mutation={CREATE_DATASET}
-      refetchQueries={[{ query: datasetListQuery }]}>
-      {(mutate, { data }) => (
-        <ADIButton onClick={mutate} className={classes.button}
-                        variant="raised" color="primary" fullWidth={true}>
-          New Dataset
-          <DescriptionIcon className={classes.rightIcon}/>
-        </ADIButton>
-      )}
-    </Mutation>
-  )
+    return (
+      <Mutation 
+        mutation={CREATE_DATASET}
+        refetchQueries={[{ query: datasetListQuery }]}>
+        {(mutate, { data }) => (
+          <ADIButton 
+            onClick={() => this.handleClick(mutate)} 
+            className={classes.button}
+            variant="raised" 
+            color="primary" 
+            fullWidth={true}>
+            New Dataset
+            <DescriptionIcon className={classes.rightIcon}/>
+          </ADIButton>
+        )}
+      </Mutation>
+    )
+  }
 }
 
 export default compose(
