a28b977247d9085a2462efe5a4f9dff62629ef4e
Cameron Mann <cameron.mann@cybera.ca>
2018-11-14 16:35:30 -0700

    Match indentation to rest of our python code

--- services/python-worker/main.py
+++ services/python-worker/main.py
@@ -15,19 +15,19 @@ channel = connection.channel()
 channel.queue_declare(queue='python-worker')
 
 def callback(ch, method, properties, body):
-    msg = body.decode('utf8')
-    print(f"Received: {msg}")
-    sys.stdout.flush()
+  msg = body.decode('utf8')
+  print(f"Received: {msg}")
+  sys.stdout.flush()
 
-    params = json.loads(msg)
-    process_path = os.path.join(WORKER_ROOT, 'tasks', f"{params['task']}.py")
+  params = json.loads(msg)
+  process_path = os.path.join(WORKER_ROOT, 'tasks', f"{params['task']}.py")
 
-    if params['task'] == 'generate' or params['task'] == 'register_transformation':
-        call([process_path, str(params['id']), str(params['ownerName'])])
-    else:
-        call([process_path, str(params['id'])])
-    print("Done")
-    sys.stdout.flush()
+  if params['task'] == 'generate' or params['task'] == 'register_transformation':
+    call([process_path, str(params['id']), str(params['ownerName'])])
+  else:
+    call([process_path, str(params['id'])])
+  print("Done")
+  sys.stdout.flush()
 
 channel.basic_consume(callback,
                       queue='python-worker',
