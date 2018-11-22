b241fd0d736282f9e6df88a64987ab46189eb8ab
Cameron Mann <cameron.mann@cybera.ca>
2018-11-21 17:02:15 -0700

    Enable serving production webpack client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,59 +1,60 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+// const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const config = {
-    entry: {
-        app: './src/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    plugins: [
-        //new CleanWebpackPlugin('dist'),
-        new HtmlWebpackPlugin({
-            title: 'Alberta Data Institute',
-            template: './src/index.html'
-        }),
-        new webpack.NamedModulesPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/, 
-                use: [ 'style-loader', 'css-loader' ] 
-            },
-            {
-                test: /\.png$/,
-                use: ['file-loader']
-            }
-        ]
-    },
-    optimization: {
-		splitChunks: {
-			cacheGroups: {
-				commons: {
-					chunks: "initial",
-					minChunks: 2,
-					maxInitialRequests: 5,
-					minSize: 0
-				},
-				vendor: {
-					test: /node_modules/,
-					chunks: "initial",
-					name: "vendor",
-					priority: 10,
-					enforce: true
-				}
-			}
-		}
-	}
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  plugins: [
+    // new CleanWebpackPlugin('dist'),
+    new HtmlWebpackPlugin({
+      title: 'Alberta Data Institute',
+      template: './src/index.html'
+    }),
+    new webpack.NamedModulesPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.png$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2,
+          maxInitialRequests: 5,
+          minSize: 0
+        },
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          priority: 10,
+          enforce: true
+        }
+      }
+    }
+  }
 };
+
 module.exports = config;
--- server/main.js
+++ server/main.js
@@ -41,7 +41,7 @@ app.use(cors())
 
 // Serve compiled version of the client from the server app's
 // dist directory (if it exists)
-// app.use(express.static("dist"))
+app.use(express.static('dist'))
 
 const authenticateUser = async ({ username, password, apikey }) => {
   let user
