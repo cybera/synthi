4e78fa2f07bbc3f74fb21402fe6358380fe3eb0d
David Ackerman <david.s.ackerman@gmail.com>
2018-12-03 01:58:16 -0700

    fix ADI-135
    Getting the API key from a query instead of the navigation context
    properly updates.

--- client/src/components/api-info/ConnectedPanel.js
+++ client/src/components/api-info/ConnectedPanel.js
@@ -2,20 +2,22 @@ import React from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-import { withNavigation } from '../../context/NavigationContext'
 import Panel from './Panel'
 
 export const datasetQuery = gql`
-query ($id: Int) {
+query DatasetAndAPIKey ($id: Int) {
   dataset(id: $id) {
     id
     name
+  }
+  currentUser {
+    apikey
   }
 }
 `
 
 const ConnectedPanel = (props) => {
-  const { id, navigation } = props
+  const { id } = props
 
   return (
     <Query query={datasetQuery} variables={{ id }}>
@@ -24,11 +26,11 @@ const ConnectedPanel = (props) => {
           if (loading) return <p>Loading...</p>
           if (error) return <p>Error!</p>
 
-          return <Panel dataset={data.dataset[0]} apikey={navigation.user.apikey} />
+          return <Panel dataset={data.dataset[0]} apikey={data.currentUser.apikey} />
         }
       }
     </Query>
   )
 }
 
-export default withNavigation(ConnectedPanel)
+export default ConnectedPanel
