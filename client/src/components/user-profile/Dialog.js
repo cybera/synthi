3cb74f9e1cf34fcbd822a1071da0f73608dbf996
Alecia Vogel <hello@alecia.ca>
2018-12-04 18:46:07 -0700

    Turn the userprofile text into read-only inputs

--- client/src/components/user-profile/Dialog.js
+++ client/src/components/user-profile/Dialog.js
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import DialogContent from '@material-ui/core/DialogContent'
 import Dialog from '@material-ui/core/Dialog'
 
-import UserProfile from './UserProfile'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogActions from '@material-ui/core/DialogActions'
+
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+
+import ADIButton from '../ADIButton'
 
 const styles = {
 }
@@ -14,7 +19,6 @@ const styles = {
 class UserProfileDialog extends React.Component {
   handleClose = () => {
     const { onClose } = this.props
-
     onClose()
   }
 
@@ -33,7 +37,31 @@ class UserProfileDialog extends React.Component {
           User Profile for {user.username}
         </DialogTitle>
         <DialogContent>
-          <UserProfile user={user} regenerateAPIKey={regenerateAPIKey} />
+          <Grid container spacing={8}>
+            <Grid item xs={6}>
+              <TextField 
+                defaultValue={user.username}
+                InputProps={{
+                  readOnly: true,
+                }}
+                margin="normal"
+                label="Username"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField 
+                defaultValue={user.apikey || " "}
+                InputProps={{
+                  readOnly: true,
+                }}
+                margin="normal"
+                label="API Key"
+                fullWidth
+              />
+            </Grid>
+            <ADIButton onClick={regenerateAPIKey}>Regenerate API Key</ADIButton>
+          </Grid>
         </DialogContent>
       </Dialog>
     );
@@ -41,9 +69,9 @@ class UserProfileDialog extends React.Component {
 }
 
 UserProfileDialog.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onClose: PropTypes.func,
-  user: PropTypes.object
+  classes: PropTypes.objectOf(PropTypes.any).isRequired,
+  onClose: PropTypes.func.isRequired,
+  user: PropTypes.objectOf(PropTypes.any).isRequired
 };
 
 export default withStyles(styles)(UserProfileDialog)
--- client/src/components/user-profile/UserProfile.js
+++ client/src/components/user-profile/UserProfile.js
@@ -1,35 +1,47 @@
 import React from 'react'
 
-import InputLabel from '@material-ui/core/InputLabel'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogActions from '@material-ui/core/DialogActions'
+
 import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
 
 import ADIButton from '../ADIButton'
+
 
 const UserProfile = (props) => {
   const { user, regenerateAPIKey } = props
   return (
     <div>
-      <Grid container spacing={24}>
-        <Grid container item xs={12}>
-          <Grid item xs={6}>
-            <InputLabel>Username</InputLabel>
+      <DialogContent>
+        <Grid container spacing={8}>
+          <Grid container item xs={6}>
+            <TextField 
+              defaultValue={user.username}
+              InputProps={{
+                readOnly: true,
+              }}
+              margin="normal"
+              label="Username"
+              fullWidth
+            />
           </Grid>
-          <Grid item xs={6}>
-            { user.username }
+          <Grid container item xs={12}>
+            <TextField 
+              defaultValue={user.apikey || " "}
+              InputProps={{
+                readOnly: true,
+              }}
+              margin="normal"
+              label="API Key"
+              fullWidth
+            />
           </Grid>
         </Grid>
-        <Grid container item xs={12}>
-          <Grid item xs={6}>
-            <InputLabel>API Key</InputLabel>
-          </Grid>
-          <Grid item xs={6}>
-            { user.apikey }
-          </Grid>
-        </Grid>
-        <Grid container item xs={12}>
-          <ADIButton onClick={regenerateAPIKey}>Regenerate API Key</ADIButton>
-        </Grid>
-      </Grid>
+      </DialogContent>
+      <DialogActions>
+        <ADIButton onClick={regenerateAPIKey}>Regenerate API Key</ADIButton>
+      </DialogActions>
     </div>
   )
 }
