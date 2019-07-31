e035ecd07c5f4f3681d4a9df7723142ca3acfdb4
David Ackerman <david.s.ackerman@gmail.com>
2019-07-31 09:46:33 -0600

    module to make sure all domain models are loaded
    Accessing the ModelFactory should probably be done from here by default
    with the exception of registering model classes. It ensures that the
    ModelFactory will know of all possible classes for Neo4J nodes.

---
+++ server/domain/models.js
@@ -0,0 +1,22 @@
+import Column from './models/column'
+import DatasetMetadata from './models/dataset-metadata'
+import Dataset from './models/dataset'
+import DatasetCSV from './models/datasetCSV'
+import Organization from './models/organization'
+import Task from './models/task'
+import Transformation from './models/transformation'
+import User from './models/user'
+
+import * as ModelFactory from './models/modelFactory'
+
+export {
+  Column,
+  DatasetMetadata,
+  Dataset,
+  DatasetCSV,
+  Organization,
+  Task,
+  Transformation,
+  User,
+  ModelFactory
+}
