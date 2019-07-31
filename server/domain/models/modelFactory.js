2cc75e8724aa64ea077dabe5dcd86bbfcdabf5f3
David Ackerman <david.s.ackerman@gmail.com>
2019-07-31 09:14:07 -0600

    ModelFactory to help reduce circular dependencies
    This also works towards the same thing DatasetFactory was doing, where
    we're able to be a bit smarter about converting Neo4J nodes into JS
    classes.
    
    Basically, every model class is registered. You can do just a basic
    version, where the class of the model chosen is the same as the Neo4J
    label. Or you can specify a different Neo4J label name and/or properties
    that should trigger the registered class over others that may match.
    Matches go from most specific to least specific.
    
    Co-authored-by: Cameron Mann <camthemann@gmail.com>

---
+++ server/domain/models/modelFactory.js
@@ -0,0 +1,75 @@
+import { containsProperties } from '../../lib/util'
+import { safeQuery } from '../../neo4j/connection'
+
+const classRegistry = {}
+const labelRegistry = {}
+
+export function register(modelClass, neo4JLabel = modelClass.name, properties = {}) {
+  classRegistry[modelClass.name] = modelClass
+  if (!(neo4JLabel in labelRegistry)) {
+    labelRegistry[neo4JLabel] = []
+  }
+  labelRegistry[neo4JLabel].push({
+    modelClassname: modelClass.name,
+    properties
+  })
+  // Every time an entry is added, which is going to be much less frequent than
+  // when they are accessed, sort the list from the most specific set of properties
+  // to the least specific.
+  labelRegistry[neo4JLabel].sort(({ properties: a }, { properties: b }) => (
+    Object.keys(b).length - Object.keys(a).length
+  ))
+}
+
+export function deriveClass(label, properties) {
+  const possibleClasses = labelRegistry[label]
+  let foundClassname = null
+  possibleClasses.forEach((modelClass) => {
+    if (!foundClassname && containsProperties(properties, modelClass.properties)) {
+      foundClassname = modelClass.modelClassname
+    }
+  })
+
+  if (foundClassname) {
+    return classRegistry[foundClassname]
+  }
+
+  throw Error(`Can't derive class for node with label: ${label}`)
+}
+
+export function derive(neo4JNode) {
+  const label = neo4JNode.labels[0]
+  const { properties } = neo4JNode
+
+  const ModelClass = deriveClass(label, properties)
+
+  return new ModelClass(neo4JNode)
+}
+
+export function getClass(datasetClassname) {
+  return classRegistry[datasetClassname]
+}
+
+export async function getByUniqueMatch(matchQuery, params) {
+  const results = await safeQuery(matchQuery, params)
+  if (!results[0]) {
+    return null
+  }
+  const result = results[0]
+  const resultKeys = Object.keys(result)
+  if (resultKeys.length !== 1) {
+    throw Error('matchQuery for getByUniqueMatch should only have one return node')
+  }
+
+  return derive(result[resultKeys[0]])
+}
+
+export async function get(id) {
+  const safeId = parseInt(id, 10)
+  const query = `
+    MATCH (node)
+    WHERE ID(node) = toInteger($id)
+    RETURN node
+  `
+  return this.getByUniqueMatch(query, { id: safeId })
+}
