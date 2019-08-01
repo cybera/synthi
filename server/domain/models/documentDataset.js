62729cbd7762cc92112197f2727c1dfac94ab1fb
David Ackerman <david.s.ackerman@gmail.com>
2019-08-01 12:27:05 -0600

    Don't try to import documents.
    I could see an argument for maybe not even having to do a real
    "transformation" to get raw text for a dataset, and this could then be
    the import step. But I'm not sure we want to really do that.
    
    If there's time, I might see what that would look like, but currently
    it's not critical to the story, and we haven't really talked about the
    consequences of putting more on that import task that we would otherwise
    do in transformations. For example, what if the import isn't quite how
    they wanted and a transformation was needed anyway? Then we're doing
    some useless steps. If they're not too costly, that may still be fine
    (and results can still be used for previews).

--- server/domain/models/documentDataset.js
+++ server/domain/models/documentDataset.js
@@ -17,6 +17,13 @@ class DocumentDataset extends Dataset {
 
     super.upload({ stream, filename })
   }
+
+  /* eslint-disable class-methods-use-this, no-unused-vars */
+  async import(removeExisting = false, options = {}) {
+    // Do nothing. Right now it doesn't really make sense to have to do
+    // an import step.
+  }
+  /* eslint-enable class-methods-use-this, no-unused-vars */
 }
 
 Dataset.ModelFactory.register(DocumentDataset, 'Dataset', { type: 'document' })
