4d60ca234878a2eb8efb0200f8b4b3937ae67b9c
David Ackerman <david.s.ackerman@gmail.com>
2019-08-14 17:08:14 -0600

    better logging of directive exceptions
    Exceptions triggered by GraphQL directives can be especially tricky to
    track down due to it being triggered by the overall GraphQL library and
    not directly by us. Making sure to get a real stack trace in those
    situations.
    
    Better yet, though the above was why I wound up here, this will catch
    any GraphQL specific errors and make sure to spit out their stacktraces.
    Apollo is actually attaching a lot of useful information to errors
    triggered during a resolver. We should probably further explore
    recommended approaches to error logging
    (see: https://www.apollographql.com/docs/apollo-server/features/metrics/#Logging),
    but for now this will at least ensure we get the stacktraces, which are
    critical for getting to where things actually went wrong.

--- server/config/winston.js
+++ server/config/winston.js
@@ -2,7 +2,13 @@ import { createLogger, transports, format } from 'winston'
 
 const myFormat = format.printf((info) => {
   if (info instanceof Error) {
-    return `${info.timestamp} ${info.level}: ${info.message}\n${info.stack}`;
+    let message = `${info.timestamp} ${info.level}: ${info.message}\n`
+    if (info.constructor.name === 'GraphQLError') {
+      message += `${info.extensions.exception.stacktrace.join('\n')}`
+    } else {
+      message += `${info.stack}`
+    }
+    return message
   }
 
   return `${info.timestamp} ${info.level}: ${info.message}`;
