8ad4bd17517efb498771a3c36fb72cad8f0ff63c
David Ackerman <david.s.ackerman@gmail.com>
2019-08-16 00:08:31 -0600

    api updates

--- test/integration/lib/dataset.py
+++ test/integration/lib/dataset.py
@@ -3,7 +3,44 @@ import io
 import os
 import requests
 
+from typing import Dict
+
 import pandas as pd
+
+def set_default_org(name=None, uuid=None, id=None):
+  assert name or uuid or id, "Must supply one of name, uuid, or id"
+
+  if uuid:
+    org_key = 'uuid'
+    org_val = uuid
+  elif name:
+    org_key = 'name'
+    org_val = name
+  elif id:
+    org_key = 'id'
+    org_val = id
+
+  info = gql_query('''
+    query {
+      currentUser {
+        organizations {
+          name
+          id
+          uuid
+        }
+      }
+    }
+  ''')
+
+  organizations = info['currentUser']['organizations']
+
+  matching_orgs = [org for org in organizations if org[org_key] == org_val]
+
+  if len(matching_orgs) != 1:
+    raise Exception(f"should match exactly one organization (matches: {len(matching_orgs)})")
+  
+  __default_org = matching_orgs[0]
+
 
 # This can take a regular graphql query (with no actual file upload) or one
 # that also includes a single upload. To modify in order to take multiple
@@ -68,15 +105,16 @@ def default_org(host=None, api_key=None):
         organizations {
           name
           id
+          uuid
         }
       }
     }
     ''')
-    __default_org = info['currentUser']['organizations'][0]['id']
+    __default_org = info['currentUser']['organizations'][0]
   
   return __default_org
 
-def get(id_or_name, raw=False, host=None, api_key=None):
+def get(id_or_name, raw=False, as_text=True, host=None, api_key=None):
   if api_key is None:
     api_key = os.environ.get('ADI_API_KEY')
   if host is None:
@@ -91,12 +129,15 @@ def get(id_or_name, raw=False, host=None, api_key=None):
   response = requests.get(f"{host}/dataset/{id}", headers=headers)
 
   if raw:
-    return response.content.decode('utf-8')
+    if as_text:
+      return response.content.decode('utf-8')
+    else:
+      return response.content
   else:
     return pd.read_csv(io.StringIO(response.content.decode('utf-8')))
 
 def meta(id_or_name, host=None, api_key=None):
-  orgid = default_org()
+  org = default_org()
 
   datasetId = None
   datasetName = None
@@ -107,8 +148,8 @@ def meta(id_or_name, host=None, api_key=None):
     datasetId = id_or_name
 
   query = '''
-  query ($orgid: Int, $datasetId: Int, $datasetName: String) {
-    dataset(org: { id: $orgid }, id: $datasetId, name: $datasetName) {
+  query ($org: OrganizationID, $datasetId: Int, $datasetName: String) {
+    dataset(org: $org, id: $datasetId, name: $datasetName) {
       id
       name
       uuid
@@ -117,7 +158,7 @@ def meta(id_or_name, host=None, api_key=None):
   '''
 
   variables = dict(
-    orgid = orgid,
+    org = org,
     datasetId = datasetId,
     datasetName = datasetName
   )
@@ -130,11 +171,11 @@ def meta(id_or_name, host=None, api_key=None):
   return results['dataset'][0]
 
 def list(host=None, api_key=None):
-  orgid = default_org()
+  org = default_org()
 
   query = '''
-  query ($orgid: Int) {
-    dataset(org: { id: $orgid }) {
+  query ($org: OrganizationID) {
+    dataset(org: $org) {
       id
       name
       uuid
@@ -143,7 +184,7 @@ def list(host=None, api_key=None):
   '''
   
   variables = dict(
-    orgid = orgid
+    org = org
   )
   
   results = gql_query(query, variables=variables, host=host, api_key=api_key)
@@ -151,7 +192,7 @@ def list(host=None, api_key=None):
   return results['dataset']
 
 def create(name, host=None, api_key=None, type=None):
-  orgid = default_org()
+  orgid = default_org()['id']
 
   query = '''
   mutation ($ownerId: Int!, $datasetName: String!, $type: DatasetType) {
@@ -166,9 +207,10 @@ def create(name, host=None, api_key=None, type=None):
   
   variables = dict(
     ownerId = orgid,
-    datasetName = name
+    datasetName = name,
+    type = type
   )
-
+    
   if type:
     variables['type'] = type
   
@@ -176,7 +218,7 @@ def create(name, host=None, api_key=None, type=None):
   
   return result['createDataset']
 
-def upload(id_or_name, file, host=None, api_key=None, type=None):
+def upload(id_or_name, file, type=None, host=None, api_key=None):
   info = meta(id_or_name, host=host, api_key=api_key)
 
   if not info:
@@ -205,43 +247,35 @@ def upload(id_or_name, file, host=None, api_key=None, type=None):
   
   return result['updateDataset']
 
-def transformation(id_or_name, path=None, code=None, host=None, api_key=None):
-  if not path and not code:
-    raise ValueError("Need to either give a path to a transformation code file or the code itself")
-  
-  if path and code:
-    raise ValueError("Give either a path to a transformation code file or code, not both")
-    
+def ensure_dataset(id_or_name, type='csv', host=None, api_key=None):
   if isinstance(id_or_name, str):
     info = meta(id_or_name, host=host, api_key=api_key)
     if not info and isinstance(id_or_name, str):
-      info = create(id_or_name, host=host, api_key=api_key)
+      info = create(id_or_name, type=type, host=host, api_key=api_key)
     elif not isinstance(id_or_name, str):
       raise ValueError("Can't create a new dataset with a numerical id")
     id = info['id']
   else:
     id = id_or_name
 
+  return id
+
+def read_code(path:str):
   if path and os.path.exists(path):
     with open(path, 'r') as file:
       code = file.read()
+  return code
+
+def transformation_basic(id_or_name, path=None, code=None, type='csv', host=None, api_key=None):
+  if not path and not code:
+    raise ValueError("Need to either give a path to a transformation code file or the code itself")
   
-  # TODO: We should probably just automatically set the computed flag if
-  # the dataset has a transformation saved to it (or consider it computed)
-  # if it simply has a transformation associated. However, to avoid making
-  # too many hasty code changes, we'll do another GraphQL call here to make
-  # it happen:
-  
-  computedQuery = '''
-  mutation UpdateDataset($id: Int!, $computed: Boolean) {
-    updateDataset(id: $id, computed: $computed) {
-      id
-      name
-    }
-  }
-  '''
-  gql_query(computedQuery, variables={'id':id, 'computed':True}, host=host, api_key=api_key)
-  
+  if path and code:
+    raise ValueError("Give either a path to a transformation code file or code, not both")
+    
+  id = ensure_dataset(id_or_name, type=type, host=host, api_key=api_key)
+  code = read_code(path)
+    
   query = '''
   mutation SaveInputTransformation($id: Int!, $code: String) {
     saveInputTransformation(id: $id, code: $code) {
@@ -259,6 +293,76 @@ def transformation(id_or_name, path=None, code=None, host=None, api_key=None):
   
   return result['saveInputTransformation']
 
+def transformation_ref(id_or_name, template:str, inputs:Dict[str,str], type='csv', host=None, api_key=None):
+  query = '''
+    mutation TemplateTransformation($output: Int!, $template: TemplateRef, $inputs: [TransformationInputMapping], $org: OrganizationID) {
+      saveInputTransformation(
+        id: $output,
+        template: $template,
+        inputs: $inputs,
+        org: $org
+      ) {
+        id
+        uuid
+        name
+      }
+    }
+  '''
+
+  org = default_org()
+
+  variables = dict(
+    output   = ensure_dataset(id_or_name, type=type, host=host, api_key=api_key),
+    template = dict(name=template),
+    inputs   = [dict(alias=k, dataset=dict(name=v)) for k,v in inputs.items()],
+    org      = org,
+  )
+
+  result = gql_query(query, variables=variables, host=host, api_key=api_key)
+
+  return result['saveInputTransformation']
+
+def transformation(id_or_name, path=None, code=None, template:str = None, inputs:Dict[str,str] = {}, type='csv', host=None, api_key=None):
+  if (path or code) and not (template or inputs):
+    return transformation_basic(id_or_name, path, code, type=type, host=host, api_key=api_key)
+  elif (template and inputs):
+    return transformation_ref(id_or_name, template, inputs, type=type, host=host, api_key=api_key)
+
+  raise Exception("Must provide code, a path, or a transformation template name and inputs")
+
+def reusable_transformation(name, path=None, code=None, inputs=[], host=None, api_key=None):
+  if not path and not code:
+    raise ValueError("Need to either give a path to a transformation code file or the code itself")
+  
+  if path and code:
+    raise ValueError("Give either a path to a transformation code file or code, not both")
+
+  org = default_org()
+
+  if path and os.path.exists(path):
+    with open(path, 'r') as file:
+      code = file.read()
+
+  query = '''
+    mutation CreateTransformationTemplate($name: String!, $inputs: [String], $code: String!, $owner: OrganizationID!) {
+      createTransformationTemplate(
+        name: $name,
+        inputs: $inputs,
+        code: $code,
+        owner: $owner
+      ) {
+        id
+        uuid
+        name
+      }
+    }
+  '''
+  
+  variables = {'name':name, 'inputs': inputs, 'code': code, 'owner': org}
+  result = gql_query(query, variables=variables, host=host, api_key=api_key)
+  
+  return result['createTransformationTemplate']
+  
 def generate(id_or_name, host=None, api_key=None):    
   if isinstance(id_or_name, str):
     info = meta(id_or_name, host=host, api_key=api_key)
