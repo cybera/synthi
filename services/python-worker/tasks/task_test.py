c3760598d0c24cdceab408afaf555072023aa19a
David Ackerman <david.s.ackerman@gmail.com>
2019-08-22 13:27:05 -0600

    test task that just sends a message back on the queue

---
+++ services/python-worker/tasks/task_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import json
+
+# get around sibling import problem
+script_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(script_dir,'..'))
+
+from common import parse_params, status_channel
+
+params = parse_params()
+
+body = {
+  "type": "task-updated",
+  "task": "task_test",
+  "taskid": params['taskid'],
+  "status": "done",
+  "message": ""
+}
+
+status_channel.basic_publish(exchange='task-status', routing_key='', body=json.dumps(body))
