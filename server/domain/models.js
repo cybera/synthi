fd2e69ce7d76d30e993f96940abe515bdec25c66
David Ackerman <david.s.ackerman@gmail.com>
2019-08-23 13:22:44 -0600

    add the TransformTask subtask to the models collection

--- server/domain/models.js
+++ server/domain/models.js
@@ -4,7 +4,7 @@ import Dataset from './models/dataset'
 import DatasetCSV from './models/datasetCSV'
 import DocumentDataset from './models/documentDataset'
 import Organization from './models/organization'
-import Task from './models/task'
+import Task, { TransformTask } from './models/task'
 import Transformation from './models/transformation'
 import User from './models/user'
 
@@ -18,6 +18,7 @@ export {
   DocumentDataset,
   Organization,
   Task,
+  TransformTask,
   Transformation,
   User,
   ModelFactory
