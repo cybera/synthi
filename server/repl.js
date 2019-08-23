12002657d5028a234f64018f27cc17b1abe01878
David Ackerman <david.s.ackerman@gmail.com>
2019-08-23 13:26:24 -0600

    populate the repl with all models in models.js
    This is instead of loading and attaching each individually. Note that I
    ran into a bit of a hiccup while doing this due to the User class's use
    of bcrypt, which needs different binaries between macos and linux.
    Previously, we'd never loaded User up in the repl, so hadn't run into
    the problem here. I don't know why the server still seemed to be
    working. In any event, if you run into the problem, you need to log into
    your server container and do an npm install. While the repl seems better
    at drawing the problem out, it has nothing to do with the actual
    problem.

--- server/repl.js
+++ server/repl.js
@@ -3,14 +3,11 @@ import config from 'config'
 import { safeQuery } from './neo4j/connection'
 import Storage from './storage'
 import * as Util from './lib/util'
-import Dataset from './domain/models/dataset'
 import Base from './domain/models/base'
-import Organization from './domain/models/organization'
 import { datasetStorageMap } from './domain/models/transformation'
 import * as ModelFactory from './domain/models/modelFactory'
-import DatasetCSV from './domain/models/datasetCSV'
-import Task, { TransformTask } from './domain/models/task'
 import DefaultQueue from './lib/queue'
+import * as Models from './domain/models'
 
 global.safeQuery = safeQuery
 global.config = config
@@ -18,14 +15,13 @@ global.Storage = Storage
 global.Util = Util
 global.neo4j = require('neo4j-driver').v1
 global.lodash = lodash
-global.Dataset = Dataset
-global.Organization = Organization
+
+// Make all model classes accessible in the global scope
+Object.assign(global, Models)
+
 global.Base = Base
 global.datasetStorageMap = datasetStorageMap
 global.ModelFactory = ModelFactory
-global.DatasetCSV = DatasetCSV
-global.Task = Task
-global.TransformTask = TransformTask
 global.DefaultQueue = DefaultQueue
 
 require('repl').start({})
