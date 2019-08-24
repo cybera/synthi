3f79ae80dd6d63cbe7839f6a134520f8e18d96da
David Ackerman <david.s.ackerman@gmail.com>
2019-08-23 18:25:33 -0600

    complete basic Task chain and TransformTask logic
    This further defines some basic bookkeeping that the Task base class
    carries out and also how its subclasses should interact with it. It
    also further defines the TransformTask subclass closer to where it will
    actually do something.
    
    datasetStorageMap is being temporarily duplicated. This version is
    changed slightly because it no longer has to deal with an array of
    transformation ids. It's also been updated to grab the uuid instead.
    When this is fully rolled out, other code should no longer need to call
    this function directly. It will only have to make a TransformationTask.
    Currently the only thing outside of generating a dataset that needs
    these mappings is download functionality, and that's because it has had
    to also do the generation up to this point (due to not being able to
    deal with coordinating work). Under the new Task system, we'll just add
    a DownloadTask as the next after a chain of TransformTasks.

--- server/domain/models/task.js
+++ server/domain/models/task.js
@@ -1,21 +1,51 @@
+import { AuthenticationError } from 'apollo-server-express'
+
 import Base from './base'
 import logger from '../../config/winston'
+
+import { safeQuery } from '../../neo4j/connection';
+import { canTransform } from '../util'
 
 import DefaultQueue from '../../lib/queue'
 
 export default class Task extends Base {
-  static async create(properties) {
-    return super.create({ ...properties, status: 'initialized' })
-  }
+  static async create(properties = {}) {
+    const { user, ...rest } = properties
+    const task = await super.create({ ...rest, status: 'initialized' })
+    if (user) {
+      await task.saveRelation(user, '<-[:SCHEDULED_BY]-')
+    }
 
-  // eslint-disable-next-line class-methods-use-this
-  async run() {
-    throw new Error('run() should be implemented in a subclass of Task')
+    return task
   }
 
   // eslint-disable-next-line class-methods-use-this, no-unused-vars
+  async run(user) {
+    throw new Error('run() should be implemented in a subclass of Task')
+  }
+
+  // eslint-disable-next-line no-unused-vars
   async done(msg) {
-    throw new Error('done() should be implemented in a subclass of Task')
+    // TODO: check for actual success before running the next task
+    this.status = 'done'
+    await this.save()
+    const nextTask = await this.next()
+    if (nextTask) {
+      await nextTask.run()
+    }
+  }
+
+  async addNext(task) {
+    await this.saveRelation(task, '<-[:NEXT]-')
+  }
+
+  // Will return null if there is no NEXT task
+  async next() {
+    return this.relatedOne('-[:NEXT]->', 'Task')
+  }
+
+  async user() {
+    return this.relatedOne('-[:SCHEDULED_BY]->', 'User')
   }
 }
 
@@ -24,24 +54,90 @@ Base.ModelFactory.register(Task)
 Task.label = 'Task'
 Task.saveProperties = ['status']
 
+/*
+  Given an array of Transformation IDs, return a mapping
+  of fully qualified dataset names to the storage location
+  that represents their input and output datasets.
+*/
+export const datasetStorageMap = async (transformation, pathType, user) => {
+  const Organization = Base.ModelFactory.getClass('Organization')
+
+  const query = `
+    MATCH (dataset:Dataset)-[ioEdge:INPUT|OUTPUT]-(t:Transformation { uuid: $transformation.uuid })
+    MATCH (org:Organization)-->(dataset)
+    RETURN dataset, org, ioEdge
+  `
+  const results = await safeQuery(query, { transformation })
+  const ioNodes = results.map(({ dataset, org, ioEdge }) => ({
+    dataset: Base.ModelFactory.derive(dataset),
+    org: new Organization(org),
+    alias: ioEdge.type === 'INPUT' ? ioEdge.properties.alias : undefined
+  }))
+
+  // This is probably going to be overly restrictive once we start allowing organizations to
+  // share an output dataset across organizational boundaries, to which other organizations
+  // can apply transformations. In that case, using similar logic as in the canTransform
+  // function, and also temporarily storing the actual transformations in the ioNodes mapping
+  // above, we could instead remove any transformations with datasets falling in the restricted
+  // space. However, since we're only allowing people to do things within organizations they
+  // are a part of, we'll take this approach for now.
+  if (!(await canTransform(user, ioNodes.map(n => n.dataset.uuid)))) {
+    throw new AuthenticationError('Cannot run a transformation without access to all the datasets involved.')
+  }
+
+  const mapping = {}
+  ioNodes.forEach(({ dataset, org, alias }) => {
+    mapping[`${org.name}:${alias || dataset.name}`] = dataset.paths[pathType]
+  })
+
+  return mapping
+}
+
 export class TransformTask extends Task {
-  static async create(properties) {
-    return super.create({ ...properties, type: 'transform' })
+  static async create(properties = {}) {
+    const { transformation, ...rest } = properties
+    const task = await super.create({ ...rest, type: 'transform' })
+    if (transformation) {
+      await task.saveRelation(transformation, '<-[:FOR]-')
+    }
+    return task
+  }
+
+  async transformation() {
+    return this.relatedOne('-[:FOR]->', 'Transformation')
   }
 
   async run() {
+    const user = await this.user()
+    const transformation = await this.transformation()
+    const storagePaths = await datasetStorageMap(transformation, 'imported', user)
+    const samplePaths = await datasetStorageMap(transformation, 'sample', user)
+
+    const outputDataset = await transformation.outputDataset()
+    const owner = await outputDataset.owner()
+    const script = await transformation.realScript()
+
+    const taskTransformationInfo = {
+      id: transformation.id,
+      script,
+      output_name: outputDataset.name,
+      owner: owner.id
+    }
+
     await DefaultQueue.sendToWorker({
       task: 'task_test',
       taskid: this.uuid,
-      status: this.status
+      status: this.status,
+      transformation: taskTransformationInfo,
+      storagePaths,
+      samplePaths
     })
   }
 
   async done(msg) {
     logger.warn(`Task ${this.uuid} completed:`)
     logger.warn('%o', msg)
-    this.status = 'done'
-    this.save()
+    await super.done(msg)
   }
 }
 
