ba6295754705671b0f3c847003499ea80d497f2a
David Ackerman <david.s.ackerman@gmail.com>
2019-08-26 13:40:17 -0600

    move TransformTask into its own file
    TransformTask was starting to get big enough to strike out on its own.
    As well, I'll be adding some more tasks that would have added to an
    overly large file.

--- server/domain/models/task.js
+++ server/domain/models/task.js
@@ -6,8 +6,6 @@ import logger from '../../config/winston'
 import { safeQuery } from '../../neo4j/connection';
 import { canTransform } from '../util'
 
-import DefaultQueue from '../../lib/queue'
-
 export default class Task extends Base {
   static async create(properties = {}) {
     const { user, ...rest } = properties
@@ -99,81 +97,10 @@ export const datasetStorageMap = async (transformation, pathType, user) => {
   return mapping
 }
 
-export class TransformTask extends Task {
-  static async create(properties = {}) {
-    const { transformation, ...rest } = properties
-    const task = await super.create({ ...rest, type: 'transform' })
-    if (transformation) {
-      await task.saveRelation(transformation, '<-[:FOR]-')
-    }
-    return task
-  }
-
-  async transformation() {
-    return this.relatedOne('-[:FOR]->', 'Transformation')
-  }
-
-  async run() {
-    const user = await this.user()
-    const transformation = await this.transformation()
-    const storagePaths = await datasetStorageMap(transformation, 'imported', user)
-    const samplePaths = await datasetStorageMap(transformation, 'sample', user)
-
-    const outputDataset = await transformation.outputDataset()
-    const owner = await outputDataset.owner()
-    const script = await transformation.realScript()
-
-    const taskTransformationInfo = {
-      id: transformation.id,
-      script,
-      output_name: outputDataset.name,
-      owner: owner.id
-    }
-
-    await DefaultQueue.sendToWorker({
-      task: 'transform',
-      // TODO: We really shouldn't need to be passing this in anymore
-      ownerName: owner.name,
-      taskid: this.uuid,
-      state: this.state,
-      transformation: taskTransformationInfo,
-      storagePaths,
-      samplePaths
-    })
-  }
-
-  async done(msg) {
-    if (msg.status === 'success') {
-      logger.warn(`Task ${this.uuid} completed:`)
-      logger.warn('%o', msg)
-      const transformation = await this.transformation()
-      const outputDataset = await transformation.outputDataset()
-
-      const { columnUpdates } = msg.data
-      if (columnUpdates) {
-        logger.warn('columnUpdates: %o', columnUpdates)
-        await outputDataset.handleColumnUpdate(columnUpdates)
-        outputDataset.sendUpdateNotification()
-      } else {
-        logger.warn(`No column updates for Dataset: ${outputDataset.name} (${outputDataset.uuid})`)
-      }
-    }
-
-    // Call super.done regardless of whether or not the task was successful
-    // to allow it to handle any sort of failure cleanup that should happen
-    // for any task.
-    await super.done(msg)
-  }
-}
-
-Base.ModelFactory.register(TransformTask, 'Task', { type: 'transform' })
-
 export async function handleGeneratedInfo(msg) {
   const Dataset = Base.ModelFactory.getClass('Dataset')
-
   logger.debug('%o', msg)
   const { datasetColumnUpdates } = msg.data
-
   Object.keys(datasetColumnUpdates).forEach(async (key) => {
     logger.debug('Column update for dataset: %s', key)
     const columns = datasetColumnUpdates[key]
---
+++ server/domain/models/transformTask.js
@@ -0,0 +1,70 @@
+import Base from './base'
+import logger from '../../config/winston'
+import DefaultQueue from '../../lib/queue'
+
+import Task, { datasetStorageMap } from './task'
+
+export default class TransformTask extends Task {
+  static async create(properties = {}) {
+    const { transformation, ...rest } = properties;
+    const task = await super.create({ ...rest, type: 'transform' });
+    if (transformation) {
+      await task.saveRelation(transformation, '<-[:FOR]-');
+    }
+    return task;
+  }
+
+  async transformation() {
+    return this.relatedOne('-[:FOR]->', 'Transformation');
+  }
+
+  async run() {
+    const user = await this.user();
+    const transformation = await this.transformation();
+    const storagePaths = await datasetStorageMap(transformation, 'imported', user);
+    const samplePaths = await datasetStorageMap(transformation, 'sample', user);
+    const outputDataset = await transformation.outputDataset();
+    const owner = await outputDataset.owner();
+    const script = await transformation.realScript();
+    const taskTransformationInfo = {
+      id: transformation.id,
+      script,
+      output_name: outputDataset.name,
+      owner: owner.id
+    };
+    await DefaultQueue.sendToWorker({
+      task: 'transform',
+      // TODO: We really shouldn't need to be passing this in anymore
+      ownerName: owner.name,
+      taskid: this.uuid,
+      state: this.state,
+      transformation: taskTransformationInfo,
+      storagePaths,
+      samplePaths
+    });
+  }
+
+  async done(msg) {
+    if (msg.status === 'success') {
+      logger.warn(`Task ${this.uuid} completed:`);
+      logger.warn('%o', msg);
+      const transformation = await this.transformation();
+      const outputDataset = await transformation.outputDataset();
+      const { columnUpdates } = msg.data;
+      if (columnUpdates) {
+        logger.warn('columnUpdates: %o', columnUpdates);
+        await outputDataset.handleColumnUpdate(columnUpdates);
+        outputDataset.sendUpdateNotification();
+      }
+      else {
+        logger.warn(`No column updates for Dataset: ${outputDataset.name} (${outputDataset.uuid})`);
+      }
+    }
+    // Call super.done regardless of whether or not the task was successful
+    // to allow it to handle any sort of failure cleanup that should happen
+    // for any task.
+    await super.done(msg);
+  }
+}
+
+Base.ModelFactory.register(TransformTask, 'Task', { type: 'transform' })
