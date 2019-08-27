b290e7ace2f3002e1c861fe2b33e22de830170a0
David Ackerman <david.s.ackerman@gmail.com>
2019-08-27 10:15:42 -0600

    get rid of prepare_download.py and weird download queue functionality
    It's hard to overstate how good this feels. We simply no longer need it.

--- server/lib/queue.js
+++ server/lib/queue.js
@@ -1,13 +1,10 @@
 import AMQPManager from 'amqp-connection-manager'
 import { pubsub } from '../graphql/pubsub'
 import * as ModelFactory from '../domain/models/modelFactory'
-import { datasetStorageMap } from '../domain/models/transformation'
 import { handleQueueUpdate } from '../domain/models/task'
 import logger from '../config/winston'
 
 const DATASET_UPDATED = 'DATASET_UPDATED'
-
-const pendingDownloads = {}
 
 const startChannel = (conn, channelName, { durable, noAck }, callback) => {
   const queueName = channelName
@@ -45,19 +42,6 @@ const startQueue = () => {
     pubsub.publish(DATASET_UPDATED, { datasetGenerated: msgJSON });
   })
 
-  startChannel(conn, 'download-status', { durable: false, noAck: true }, (msg) => {
-    const msgJSON = JSON.parse(msg.content.toString())
-
-    // TODO: Create a unique download ID
-    const pendingDownloadCallback = pendingDownloads[`${msgJSON.id}`]
-    if (pendingDownloadCallback) {
-      pendingDownloads[`${msgJSON.id}`] = undefined
-      pendingDownloadCallback()
-    }
-
-    logger.info(msgJSON)
-  })
-
   startChannel(conn, 'task-status', { durable: false, noAck: true }, async (msg) => {
     const msgJSON = JSON.parse(msg.content.toString())
 
@@ -85,26 +69,6 @@ class AMQP {
 
   async sendToWorker(msg) {
     await this.worker.sendToQueue('python-worker', msg)
-  }
-
-  async prepareDownload(dataset, user, callback) {
-    // TODO: Pass a unique download ID (have tasks send JSON as argument)
-    const owner = await dataset.owner()
-    const transformations = await dataset.parentTransformations()
-    const storagePaths = await datasetStorageMap(transformations.map(t => t.id), 'imported', user)
-    const samplePaths = await datasetStorageMap(transformations.map(t => t.id), 'sample', user)
-
-    await this.sendToWorker({
-      task: 'prepare_download',
-      id: dataset.id,
-      ownerName: owner.name,
-      transformations,
-      storagePaths,
-      samplePaths
-    })
-
-    // TODO: Create a unique download ID
-    pendingDownloads[`${dataset.id}`] = callback
   }
 
   close() {
