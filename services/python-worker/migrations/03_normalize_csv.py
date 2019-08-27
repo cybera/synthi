3073dd763ee9d78017cb7857b5b60a7fb733e365
David Ackerman <david.s.ackerman@gmail.com>
2019-08-27 14:20:19 -0600

    fix broken migration
    Migrations still need database connections. It does make less and less
    sense for them to use the worker container to operate, though. In the
    meantime, this restores what this migration needs, and it still works,
    though we've been a pretty long time from having data that would need
    the migration.

--- services/python-worker/migrations/03_normalize_csv.py
+++ services/python-worker/migrations/03_normalize_csv.py
@@ -4,8 +4,24 @@ import os, sys
 script_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(script_dir,'..'))
 
-from tasks.import_csv import store_csv
 import storage
+
+# When importing, we take a sample of the dataset's rows and store that
+# as well. For really small datasets, this could be the whole thing, and
+# we may want to consider a much higher number. The most important thing
+# is to keep it small enough to cut down on the transfer time when bringing
+# it straight from object storage.
+SAMPLE_SIZE=100
+
+def store_csv(df, dataset):
+  # Write out normalized versions of the CSV file. These will have header
+  # rows, even if the original data has none (auto-generated headers will
+  # be generic: 'Column_1', 'Column_2', etc.). This makes it easier for
+  # anything reading this data, as it can assume a single way of storing
+  # CSV files that we can't assume during the import process.
+  sample_size = min(df.shape[0], SAMPLE_SIZE)
+  storage.write_csv(df, f"{dataset['uuid']}/imported.csv")
+  storage.write_csv(df.sample(sample_size), f"{dataset['uuid']}/sample.csv")
 
 def migrate(tx):
   results = tx.run('MATCH (d:Dataset) RETURN d')
