11f965ce347a506ed5eadc70b9bad8decc64c84e
David Ackerman <david.s.ackerman@gmail.com>
2019-08-27 16:32:19 -0600

    handleQueueUpdate in task.js no longer needed
    Updates are coordinated through individual tasks now.

--- server/domain/models/task.js
+++ server/domain/models/task.js
@@ -113,24 +113,3 @@ export async function handleGeneratedInfo(msg) {
     await dataset.handleColumnUpdate(columns)
   })
 }
-
-export async function handleQueueUpdate(msgJSON) {
-  const Dataset = Base.ModelFactory.deriveClass('Dataset', { type: 'csv' })
-
-  if (msgJSON.type === 'dataset-updated'
-      && msgJSON.task === 'generate'
-      && msgJSON.status === 'success') {
-    await handleGeneratedInfo(msgJSON)
-  } else {
-    const dataset = await Dataset.get(msgJSON.id)
-    // TODO:
-    // 1. We shouldn't just blindly trust this message. One way of dealing with the
-    //    trust is to send a token along with the original queue message and expect
-    //    that to come back to confirm.
-    // 2. Additionally, there should be some sort of Task intermediary. It would
-    //    provide an extra level of narrowing the focus, as Datasets could go back
-    //    to not caring about events directly from outside like this. It would also
-    //    narrow attack vectors to a Task currently unfinished, vs any dataset.
-    await dataset.handleQueueUpdate(msgJSON)
-  }
-}
