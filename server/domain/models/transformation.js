4f6ed7abdd473df8982e29088f59c8c43f7dfd28
David Ackerman <david.s.ackerman@gmail.com>
2019-08-27 18:38:55 -0600

    remove now unused datasetStorageMap
    This is the version that works over several transformation ID values.
    An updated version just does it for one transformation and uses the
    uuid.

--- server/domain/models/transformation.js
+++ server/domain/models/transformation.js
@@ -1,10 +1,8 @@
 import shortid from 'shortid'
 import waitFor from 'p-wait-for'
 
-import { AuthenticationError } from 'apollo-server-express'
-
 import { fullScriptPath } from '../../lib/util'
-import { memberOfOwnerOrg, canTransform } from '../util'
+import { memberOfOwnerOrg } from '../util'
 import Storage from '../../storage'
 import Base from './base'
 import logger from '../../config/winston'
@@ -113,51 +111,12 @@ class Transformation extends Base {
       SET dataset.generating = false
     `
     await safeQuery(query, { transformation: this, error })
-}
+  }
 }
 
 Transformation.label = 'Transformation'
 Transformation.saveProperties = ['script', 'name']
 
 Base.ModelFactory.register(Transformation)
-
-/*
-  Given an array of Transformation IDs, return a mapping
-  of fully qualified dataset names to the storage location
-  that represents their input and output datasets.
-*/
-export const datasetStorageMap = async (transformationIds, pathType, user) => {
-  const Organization = Base.ModelFactory.getClass('Organization')
-
-  const query = `
-    MATCH (org:Organization)-->(dataset:Dataset)-[ioEdge:INPUT|OUTPUT]-(t:Transformation)
-    WHERE ID(t) IN $transformationIds
-    RETURN dataset, org, ioEdge
-  `
-  const results = await safeQuery(query, { transformationIds })
-  const ioNodes = results.map(({ dataset, org, ioEdge }) => ({
-    dataset: Base.ModelFactory.derive(dataset),
-    org: new Organization(org),
-    alias: ioEdge.type === 'INPUT' ? ioEdge.properties.alias : undefined
-  }))
-
-  // This is probably going to be overly restrictive once we start allowing organizations to
-  // share an output dataset across organizational boundaries, to which other organizations
-  // can apply transformations. In that case, using similar logic as in the canTransform
-  // function, and also temporarily storing the actual transformations in the ioNodes mapping
-  // above, we could instead remove any transformations with datasets falling in the restricted
-  // space. However, since we're only allowing people to do things within organizations they
-  // are a part of, we'll take this approach for now.
-  if (!(await canTransform(user, ioNodes.map(n => n.dataset.uuid)))) {
-    throw new AuthenticationError('Cannot run a transformation without access to all the datasets involved.')
-  }
-
-  const mapping = {}
-  ioNodes.forEach(({ dataset, org, alias }) => {
-    mapping[`${org.name}:${alias || dataset.name}`] = dataset.paths[pathType]
-  })
-
-  return mapping
-}
 
 export default Transformation
--- server/repl.js
+++ server/repl.js
@@ -4,7 +4,6 @@ import { safeQuery } from './neo4j/connection'
 import Storage from './storage'
 import * as Util from './lib/util'
 import Base from './domain/models/base'
-import { datasetStorageMap } from './domain/models/transformation'
 import * as ModelFactory from './domain/models/modelFactory'
 import DefaultQueue from './lib/queue'
 import * as Models from './domain/models'
@@ -20,7 +19,6 @@ global.lodash = lodash
 Object.assign(global, Models)
 
 global.Base = Base
-global.datasetStorageMap = datasetStorageMap
 global.ModelFactory = ModelFactory
 global.DefaultQueue = DefaultQueue
 
