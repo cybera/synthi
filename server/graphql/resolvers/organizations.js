713107163c9b65e8564d5a048f3e3ecd9a79b785
Cameron Mann <cameron.mann@cybera.ca>
2019-08-29 16:51:26 -0600

    Orgs: Remove last references to old repository and remove it

--- server/graphql/resolvers/organizations.js
+++ server/graphql/resolvers/organizations.js
@@ -1,9 +1,7 @@
-import OrganizationRepository from '../../domain/repositories/organizationRepository'
-
 export default {
   Organization: {
     organization({ context }) {
-      return OrganizationRepository.getAll(context)
+      return context.user.orgs()
     },
   },
 }
