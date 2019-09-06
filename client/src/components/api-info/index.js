c0f84f32296224551df05d0e602bb0dc0e278df8
Alecia Vogel <hello@alecia.ca>
2019-09-06 10:50:12 -0600

    Update index to export other modules from /api-info

--- client/src/components/api-info/index.js
+++ client/src/components/api-info/index.js
@@ -1,3 +1,13 @@
 import ConnectedPanel from './ConnectedPanel'
+import Panel from './Panel'
+import CodeBlocks from './CodeBlocks'
+import CodeBlockTabs from './CodeBlockTabs'
 
 export default ConnectedPanel
+
+export {
+  ConnectedPanel,
+  Panel,
+  CodeBlocks,
+  CodeBlockTabs
+}
