cc5e471bc22004ceec1edd120f9da52d130f33ee
Jeremy Nickurak <jeremy@nickurak.ca>
2019-09-13 09:03:53 -0600

    Testenv: switch to using pytest to run our test
    This will still allow a test that fails due to a python exception to
    be caught, but for the moment doesn't enforce that test outcomes are
    actually correct.
    
    Note that since (for the moment) we're only testing by manually
    reviewing the output of the tests, we're explicitly disabling pytest's
    output capturing.

--- test/integration/main.py
+++ test/integration/main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import pytest
 
 os.environ['ADI_API_KEY'] = 'test-token'
 os.environ['ADI_API_HOST'] = 'http://server:3000'
@@ -8,55 +9,57 @@ from lib import dataset
 
 dataset.set_default_org('test')
 
-print("Testing upload with iris dataset...")
+def test_adi(capsys):
+  with capsys.disabled():
+    print("Testing upload with iris dataset...")
 
-dataset.upload('iris', 'data/iris.csv')
-print(dataset.list())
-iris = dataset.get('iris')
-print(iris.head())
+    dataset.upload('iris', 'data/iris.csv')
+    print(dataset.list())
+    iris = dataset.get('iris')
+    print(iris.head())
 
-print("Testing computed dataset operations...")
-dataset.transformation('iris_means', 'data/iris_means.py')
-iris_means = dataset.get('iris_means')
-iris_means.head()
-print(iris_means.head())
+    print("Testing computed dataset operations...")
+    dataset.transformation('iris_means', 'data/iris_means.py')
+    iris_means = dataset.get('iris_means')
+    iris_means.head()
+    print(iris_means.head())
 
-print("Testing regular csv upload, using a type...")
-dataset.upload('iris_with_type', 'data/iris.csv', type='csv')
-iris_with_type = dataset.get('iris')
-print(iris_with_type.head())
+    print("Testing regular csv upload, using a type...")
+    dataset.upload('iris_with_type', 'data/iris.csv', type='csv')
+    iris_with_type = dataset.get('iris')
+    print(iris_with_type.head())
 
-print("Testing document upload, using a type...")
-dataset.upload('txt_document', 'data/test.txt', type='document')
-txt_document = dataset.get('txt_document', raw=True)
-print(txt_document)
+    print("Testing document upload, using a type...")
+    dataset.upload('txt_document', 'data/test.txt', type='document')
+    txt_document = dataset.get('txt_document', raw=True)
+    print(txt_document)
 
-print("Testing creation of a reusable transformation...")
-dataset.upload('iris-testing-1', 'data/iris.csv')
-dataset.upload('iris-testing-2', 'data/iris.csv')
-dataset.reusable_transformation('IrisMeans', 'data/iris_means.py', inputs=['iris'])
-dataset.transformation(
-    'iris-testing-means-1',
-    template = 'IrisMeans',
-    inputs = {
-        'iris': 'iris-testing-1'
-    }
-)
-df = dataset.get('iris-testing-means-1')
-print(df.head())
+    print("Testing creation of a reusable transformation...")
+    dataset.upload('iris-testing-1', 'data/iris.csv')
+    dataset.upload('iris-testing-2', 'data/iris.csv')
+    dataset.reusable_transformation('IrisMeans', 'data/iris_means.py', inputs=['iris'])
+    dataset.transformation(
+        'iris-testing-means-1',
+        template = 'IrisMeans',
+        inputs = {
+            'iris': 'iris-testing-1'
+        }
+    )
+    df = dataset.get('iris-testing-means-1')
+    print(df.head())
 
-dataset.transformation(
-    'iris-testing-means-2',
-    template = 'IrisMeans',
-    inputs = {
-        'iris': 'iris-testing-2'
-    }
-)
-df = dataset.get('iris-testing-means-2')
-print(df.head())
+    dataset.transformation(
+        'iris-testing-means-2',
+        template = 'IrisMeans',
+        inputs = {
+            'iris': 'iris-testing-2'
+        }
+    )
+    df = dataset.get('iris-testing-means-2')
+    print(df.head())
 
-print("Clearing test environment...")
+    print("Clearing test environment...")
 
-for d in dataset.list():
-    dataset.delete(d['uuid'])
+    for d in dataset.list():
+        dataset.delete(d['uuid'])
 
