ed3d33708f15eafd81a44b6d036c70314678b9b2
Cameron Mann <cameron.mann@cybera.ca>
2019-09-16 13:49:00 -0600

    Server: Convert connection.js to typescript

---
+++ server/neo4j/connection.ts
@@ -0,0 +1,40 @@
+import config from 'config'
+import logger from '../config/winston'
+
+import _neo4j = require('neo4j-driver')
+const neo4j = _neo4j.v1
+
+interface Neo4jConfig {
+  url: string
+  username: string
+  password: string
+}
+
+interface Indexable {
+  [index: string]: any
+}
+
+const connectionInfo: Neo4jConfig = config.get('neo4j')
+const neo4jConnection = neo4j.driver(
+  connectionInfo.url,
+  neo4j.auth.basic(connectionInfo.username, connectionInfo.password),
+  { disableLosslessIntegers: true }
+)
+
+const safeQuery = async (query: string, params: object): Promise<Indexable[]> => {
+  const session = neo4jConnection.session()
+  let records: object[] = []
+
+  try {
+    const result = await session.run(query, params)
+    records = result.records.map(record => record.toObject())
+  } catch (err) {
+    logger.error(err)
+  }
+
+  session.close()
+  return records
+}
+
+export { safeQuery, neo4j, Indexable }
+export default neo4jConnection
