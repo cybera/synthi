c87533fea3a2833de59e6958668125f94563ef43
David Ackerman <david.s.ackerman@gmail.com>
2019-10-03 10:03:31 -0600

    make Query a bit more flexible
    If we want to return multiple nodes in our results, Query will map them
    to the actual model classes.

--- server/neo4j/query.js
+++ server/neo4j/query.js
@@ -1,9 +1,28 @@
 import { safeQuery } from './connection'
 import * as ModelFactory from '../domain/models/modelFactory'
+import { reduce, isArray } from 'lodash'
+
+export const SINGLE_NODES = query => result => result[query.returnRefs[0]]
+export const NODE_MAP     = _     => result => result
 
 class Query {
-  constructor(mainReturnRef) {
-    this.mainReturnRef = mainReturnRef
+  constructor(returnRefOrRefs, resultMapper) {
+    if (isArray(returnRefOrRefs)) {
+      this.returnRefs = returnRefOrRefs
+      // If we have more than one return reference, we probably want our rows
+      // to be maps of the nodes, with keys the same as the references we 
+      // passed in.
+      this.resultMapper = NODE_MAP
+    } else {
+      this.returnRefs = [returnRefOrRefs]
+      // If we have only a single, non-array of return references, we probably
+      // only want a list of single nodes
+      this.resultMapper = SINGLE_NODES
+    }
+    // If we're explicitly specifying a result mapper, use that
+    if (resultMapper) {
+      this.resultMapper = resultMapper
+    }
     this.parts = []
     this.parameters = {}
   }
@@ -23,17 +42,23 @@ class Query {
       }
       return strOrFunc
     })
-    strs.push(`RETURN ${this.mainReturnRef}`)
+    strs.push(`RETURN ${this.returnRefs.join(', ')}`)
     return strs.join('\n')
   }
 
-  async run(extraParams) {
+  async run(extraParams, resultMapper = this.resultMapper) {
     const finalParameters = {}
     Object.assign(finalParameters, this.parameters)
     Object.assign(finalParameters, extraParams)
     const results = await safeQuery(this.toString(finalParameters), finalParameters)
 
-    return results.map(result => ModelFactory.derive(result[this.mainReturnRef]))
+    return results.map(result => {
+      const accumulator = (acc, key) => {
+        acc[key] = ModelFactory.derive(result[key])
+        return acc
+      }
+      return reduce(Object.keys(result), accumulator, {})
+    }).map(resultMapper(this))
   }
 }
 
