031fd89f29142122ba77289878553e58d6a2d360
David Ackerman <david.s.ackerman@gmail.com>
2019-11-15 17:13:51 -0700

    transformations browser: more advanced filtering
    This borrows a lot from the more advanced Dataset browser filtering. It
    also adds a new index for transformation text searches.

--- client/src/components/transformations/FilterPanel.js
+++ client/src/components/transformations/FilterPanel.js
@@ -1,23 +1,39 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { makeStyles } from '@material-ui/styles'
-import Checkbox from '@material-ui/core/Checkbox'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Typography from '@material-ui/core/Typography'
+import {
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  Grid,
+  TextField,
+} from '@material-ui/core'
+import ChipInput from 'material-ui-chip-input'
 
 import TransformationFilterContext from '../../contexts/TransformationFilterContext'
+
+import ADIButton from '../layout/buttons/ADIButton'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(2.5),
     marginBottom: theme.spacing(1),
-    marginLeft: theme.spacing(2.5),
-    marginRight: theme.spacing(2.5),
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(0),
   },
   title: {
     fontSize: 18,
   },
+  optionGrid: {
+    width: '100%'
+  },
+  filterRow: {
+    width: '100%'
+  },
+  filterButton: {
+    width: '100%'
+  }
 }))
 
 const ContextCheckbox = ({ label, value, setFunction }) => (
@@ -41,24 +57,72 @@ ContextCheckbox.propTypes = {
 }
 
 const FilterPanel = () => {
-  const filter = useContext(TransformationFilterContext)
+  const { filter: currentFilter, updateFilter } = useContext(TransformationFilterContext)
+  const [filter, setFilter] = useState(currentFilter)
+  const changeFilter = (name) => (value) => setFilter({ ...filter, [name]: value })
   const classes = useStyles()
+
+  const filterSubmitOnEnter = (event) => {
+    if (event.key === 'Enter') {
+      updateFilter(filter)
+    }
+  }
 
   return (
     <div className={classes.root}>
       <Typography className={classes.title} color="textSecondary" gutterBottom>
         Filter Options
       </Typography>
-      <ContextCheckbox
-        label="Published Only"
-        value={filter.publishedOnly}
-        setFunction={filter.setPublishedOnly}
-      />
-      <ContextCheckbox
-        label="From other organizations"
-        value={filter.includeShared}
-        setFunction={filter.setIncludeShared}
-      />
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        className={classes.optionGrid}
+        onKeyPress={filterSubmitOnEnter}
+      >
+        <Grid item className={classes.filterRow}>
+          <TextField
+            id="outlined-search"
+            label="Search"
+            type="search"
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            value={filter.searchString}
+            onChange={(e) => changeFilter('searchString')(e.target.value)}
+          />
+        </Grid>
+        <Grid item className={classes.filterRow}>
+          <ContextCheckbox
+            label="Published Only"
+            value={filter.publishedOnly}
+            setFunction={changeFilter('publishedOnly')}
+          />
+          <ContextCheckbox
+            label="From other organizations"
+            value={filter.includeShared}
+            setFunction={changeFilter('includeShared')}
+          />
+        </Grid>
+        <Grid item className={classes.filterRow}>
+          <ChipInput
+            onChange={changeFilter('tags')}
+            defaultValue={filter.tags}
+            margin="normal"
+            fullWidth
+            fullWidthInput
+            label="Tags"
+          />
+        </Grid>
+        <Grid item className={classes.filterRow}>
+          <ADIButton
+            onClick={() => updateFilter(filter)}
+            className={classes.filterButton}
+          >
+            Filter
+          </ADIButton>
+        </Grid>
+      </Grid>
     </div>
   )
 }
