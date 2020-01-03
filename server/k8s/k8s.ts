d70c8c967ff537d6b76417b48f8cff109d7b396e
Cameron Mann <cameron.mann@cybera.ca>
2020-01-03 16:59:29 -0700

    Interim: Convert msg to env

--- server/k8s/k8s.ts
+++ server/k8s/k8s.ts
@@ -1,5 +1,7 @@
 import * as k8s from '@kubernetes/client-node'
 
+import { Task } from '../domain/models'
+
 const kc = new k8s.KubeConfig()
 kc.loadFromFile('/home/cameron/devel/adi/server/config/kubeconfig')
 
@@ -8,15 +10,34 @@ const k8sApi = kc.makeApiClient(k8s.BatchV1Api)
 class JobConfig {
   image: string
   command: string[]
-  args: string[]
+  args?: string[]
+  message?: {[key: string]: any}
+}
+
+function msgToEnv(msg: {[key: string]: any}, prefix = '_'): k8s.V1EnvVar[] {
+  return Object.keys(msg).flatMap((key) => {
+    const value = msg[key]
+
+    if (typeof value === 'object') {
+      return msgToEnv(value, `${prefix}${key}_`)
+    }
+
+    const ev = new k8s.V1EnvVar()
+    ev.name = `ADI_PARAMS${prefix}${key}`
+    ev.value = String(value)
+    return ev
+  })
 }
 
 function runJob(config: JobConfig): void {
   const container = new k8s.V1Container()
   container.name = 'worker'
-  container.image = config.image || 'continuumio/miniconda'
+  container.image = config.image
   container.command = config.command
   container.args = config.args
+  if (config.message) {
+    container.env = msgToEnv(config.message)
+  }
 
   const podSpec = new k8s.V1PodSpec()
   podSpec.containers = [container]
