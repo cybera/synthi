6a7136a555204b4db9226013bbfea54f32cb08fc
Cameron Mann <cameron.mann@cybera.ca>
2020-02-10 17:16:12 -0700

    DB: Move migrations from python-worker to server
    Move migrations from being run as python scripts on the python-worker to
    being run as typescript scripts on the sever. With workers now being run
    in kubernetes, removing their remaining utility functionality will allow
    us to remove them from the docker environment completely and simplify
    deployments. This commit also removes a number of outdated migrations
    that are no longer needed.

--- services/python-worker/migrations/00_tags.py
+++
@@ -1,9 +0,0 @@
-# This script adds basic system tags to the database
-
-def migrate(tx):
-  tx.run('''
-    MERGE (:Tag { name: 'Integer', system: true })
-    MERGE (:Tag { name: 'Float', system: true })
-    MERGE (:Tag { name: 'String', system: true })
-    MERGE (:Tag { name: 'Boolean', system: true })
-  ''')
