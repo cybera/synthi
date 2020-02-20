f32ac33fbf768d0cdf9d9fb01a53faf487439c1d
Shawn Ayotte <shawn.ayotte@cybera.ca>
2020-02-20 16:08:20 -0700

    Added helper script to restore backups

--- bin/restore.sh
+++ bin/restore.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
-# restore.sh - choose and restore a backup on the neo4j container.
-# Store menu options selected by the user
-INPUT=./menu.sh.$$
-
-# Storage file for displaying cal and date command output
-OUTPUT=./output.sh.$$
-
-# trap and delete temp files
-# trap "rm $OUTPUT; rm $INPUT; exit" SIGHUP SIGINT SIGTERM
-
-
-IN=$(docker exec -it adi_backups_1 ./backup.py list)
-# arrIN=(${IN//\n/ })
-# echo $arrIN
-while true
+# docker exec -it adi_backups_1 ./backup.py list
+clear
+PS3='Backup Container: '
+echo "Please select the container used to run backups (ctl-d to exit)"
+select backup_cont in $(docker ps --format "{{.Names}}")
 do
 
-### display main menu ###
-dialog  --backtitle "ADI Backup Restore" \
---title "[ M A I N - M E N U ]" \
---menu "You can use the UP/DOWN arrow keys, the first \n\
-letter of the choice as a hot key, or the \n\
-number keys 1-9 to choose an option.\n\
-Choose the TASK" 15 50 4 \
-$IN 2>"${INPUT}"
-
-menuitem=$(<"${INPUT}")
-
-# echo $menuitem
-# make decsion
-# case $menuitem in
-# 	List) list_backups;;
-# 	Exit) echo "Bye"; break;;
-# esac
-
+    PS3='Backup: '
+    echo "Please choose a backup (ctrl-D to exit)"
+    select backup in $(docker exec -it $backup_cont ./backup.py list)
+    do
+        PS3='Restore backup to: '
+        echo "Select the container to restore to -Neo4J or OngDB- (ctl-d to exit)"
+        select restore_cont in $(docker ps -a --format "{{.Names}}")
+        do
+            echo "Stopping $restore_cont"
+            docker stop $restore_cont
+            echo "Restoring backup"
+            trim_backup=${backup/$'\r'}
+            # echo "Trimmed: $trim_backup"
+            docker exec -it $backup_cont ./backup.py restore $trim_backup
+            # echo "Length ${#trim_backup}"
+            back_folder=${trim_backup::29-3}
+            echo "backup folder: $back_folder"
+            docker run -it --mount type=volume,src=adi_backup,dst=/backup,volume-driver=local cybera/adi-neo4j:latest  neo4j-admin restore --from=/backup/torestore/$back_folder --force
+            echo "Starting $restore_cont "
+            docker start $restore_cont
+            echo "Restore complete"
+            exit 1
+        done
+    done
 done
-echo $menuitem
-# if temp files found, delete em
-# [ -f $OUTPUT ] && rm $OUTPUT
-# [ -f $INPUT ] && rm $INPUT
